34606a3d1e0b304a858af5298daefc762ef6f419
Shivam Singh <shivampesitbng@gmail.com>
2017-07-31 23:42:20 -0700

    direct_bus

--- src/components/6_DIRECT_BUS.vue
+++ src/components/6_DIRECT_BUS.vue
@@ -26,13 +26,48 @@ export default{
           return response.json();
         })
         .then(bus_routes => {
-          console.log("bus routes -> " + bus_routes);
+          //console.log("bus routes -> " + bus_routes);
           //callback -> get_buses_on_route
-          callback_get_buses_on_route(bus_routes);
+          for(let i in bus_routes){
+            callback_get_buses_on_route(bus_routes[i]);
+          }
         });
     },
-    get_buses_on_route(x){
-      console.log("calling -> get_buses_on_route -> " + x)
+    get_buses_on_route(bus_route){
+      //console.log("calling -> get_buses_on_route -> " + bus_route);
+      this.$http.get('buses_on_route/'+bus_route+'.json')
+        .then(response => {
+          return response.json();
+        })
+        .then(buses_up => {
+          //console.log("up -> " + buses_up);
+          for(var i in buses_up){
+            console.log(buses_up[i]+"-UP"); //** buses-up
+          }
+          //
+          //console.log("_pehle -> "+bus_route);
+          let x = bus_route.indexOf("-");
+          //console.log("index of '-' ->" + x);
+          var str2 = bus_route.slice(x+1);
+          //console.log(str2);
+          var str1 = bus_route.slice(0,x);
+          //console.log(str1);
+          var str3 = str2 + '-' + str1;
+          //console.log("_baad me -> "+str3);
+          //
+          if(buses_up == null){
+            this.$http.get('buses_on_route/'+str3+'.json')
+              .then(response => {
+                return response.json();
+              })
+              .then(buses_dn => {
+                //console.log("dn -> "+buses_dn);
+                for(let i in buses_dn){
+                  console.log(buses_dn[i]+"-DN"); //** buses-dn
+                }
+              })
+            }
+        })
     }
   },
   computed:{
@@ -44,7 +79,7 @@ export default{
     this.get_bus_routes(this.get_buses_on_route);
   },
 }
-</script>
 
+</script>
 <style scoped>
 </style>
