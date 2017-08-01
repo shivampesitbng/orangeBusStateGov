f553479acced36cafa98f6b675931cd511cf3b40
Shivam Singh <shivampesitbng@gmail.com>
2017-08-01 06:16:43 -0700

    bug fixed direct bus route

--- src/components/7_SHOW_DIRECT_BUS_ROUTE.vue
+++ src/components/7_SHOW_DIRECT_BUS_ROUTE.vue
@@ -49,6 +49,7 @@ export default{
             //
 
             this.$http.get('bus_routes/'+str3+'.json')
+
               .then(response => {
                 return response.json();
               })
@@ -62,77 +63,106 @@ export default{
 
                 if(str == 'UP'){
                   //console.log("UP");
+                  let src_flg = false , dest_flg = false ;
                   for(let r in show_route){
                     if(this.$store.state.selected_source.name == show_route[r]){
                       console.log("source -> " + show_route[r]);
                       this.stop_name_arr.push("source -> " + show_route[r]);
+                      src_flg = true;
                       continue;
                     }
                     if(this.$store.state.selected_destination.name == show_route[r]){
                       console.log("destination -> " + show_route[r]);
                       this.stop_name_arr.push("destination -> " + show_route[r]);
+                      dest_flg = true;
                       continue;
                     }
-                    console.log(show_route[r]);
-                    this.stop_name_arr.push(show_route[r]);
+                    if(src_flg == true && dest_flg == true)
+                      break;
+                    if(src_flg == true){
+                      console.log(show_route[r]);
+                      this.stop_name_arr.push(show_route[r]);
+                    }
                   }
                 }else if(str == 'DN'){
                   //console.log("DN");
+                  let src_flg = false , dest_flg = false ;
                   for(let r = show_route.length-1; r >= 0; r-- ){
                     if(this.$store.state.selected_source.name == show_route[r]){
                       console.log("source -> " + show_route[r]);
                       this.stop_name_arr.push("source -> " + show_route[r]);
+                      src_flg = true;
                       continue;
                     }
                     if(this.$store.state.selected_destination.name == show_route[r]){
                       console.log("destination -> " + show_route[r]);
                       this.stop_name_arr.push("destination -> " + show_route[r]);
+                      dest_flg = true;
                       continue;
                     }
-                    console.log(show_route[r]);
-                    this.stop_name_arr.push(show_route[r]);
+                    if(src_flg == true && dest_flg == true)
+                      break;
+                    if(src_flg == true){
+                      console.log(show_route[r]);
+                      this.stop_name_arr.push(show_route[r]);
+                    }
                   }
                 }
                 return ;
               })
           }
+          else if(show_route != null){
+            //
+            let x = bus.no.indexOf("-");
+            let str = bus.no.slice(x+1);
+            //console.log("str -> "+str);
 
-          //
-          let x = bus.no.indexOf("-");
-          let str = bus.no.slice(x+1);
-          //console.log("str -> "+str);
-
-          if(str == 'UP'){
-            //console.log("UP");
-            for(let r in show_route){
-              if(this.$store.state.selected_source.name == show_route[r]){
-                console.log("source -> " + show_route[r]);
-                this.stop_name_arr.push("source -> " + show_route[r]);
-                continue;
+            if(str == 'UP'){
+              //console.log("UP");
+              let src_flg = false , dest_flg = false ;
+              for(let r in show_route){
+                if(this.$store.state.selected_source.name == show_route[r]){
+                  console.log("source -> " + show_route[r]);
+                  this.stop_name_arr.push("source -> " + show_route[r]);
+                  src_flg = true;
+                  continue;
+                }
+                if(this.$store.state.selected_destination.name == show_route[r]){
+                  console.log("destination -> " + show_route[r]);
+                  this.stop_name_arr.push("destination -> " + show_route[r]);
+                  dest_flg = true;
+                  continue;
+                }
+                if(src_flg == true && dest_flg == true)
+                  break;
+                if(src_flg == true){
+                  console.log(show_route[r]);
+                  this.stop_name_arr.push(show_route[r]);
+                }
               }
-              if(this.$store.state.selected_destination.name == show_route[r]){
-                console.log("destination -> " + show_route[r]);
-                this.stop_name_arr.push("destination -> " + show_route[r]);
-                continue;
+            }else if(str == 'DN'){
+              //console.log("DN");
+              let src_flg = false , dest_flg = false ;
+              for(let r = show_route.length; r >= 0; r-- ){
+                if(this.$store.state.selected_source.name == show_route[r]){
+                  console.log("source -> " + show_route[r]);
+                  this.stop_name_arr.push("source -> " + show_route[r]);
+                  src_flg = true;
+                  continue;
+                }
+                if(this.$store.state.selected_destination.name == show_route[r]){
+                  console.log("destination -> " + show_route[r]);
+                  this.stop_name_arr.push("destination -> " + show_route[r]);
+                  dest_flg = true;
+                  continue;
+                }
+                if(src_flg == true && dest_flg == true)
+                  break;
+                if(src_flg == true){
+                  console.log(show_route[r]);
+                  this.stop_name_arr.push(show_route[r]);
+                }
               }
-              console.log(show_route[r]);
-              this.stop_name_arr.push(show_route[r]);
-            }
-          }else if(str == 'DN'){
-            //console.log("DN");
-            for(let r = show_route.length; r >= 0; r-- ){
-              if(this.$store.state.selected_source.name == show_route[r]){
-                console.log("source -> " + show_route[r]);
-                this.stop_name_arr.push("source -> " + show_route[r]);
-                continue;
-              }
-              if(this.$store.state.selected_destination.name == show_route[r]){
-                console.log("destination -> " + show_route[r]);
-                this.stop_name_arr.push("destination -> " + show_route[r]);
-                continue;
-              }
-              console.log(show_route[r]);
-              this.stop_name_arr.push(show_route[r]);
             }
           }
         })
