31797e0e0add34b25d74fc70703fd8af83cee315
Shivam Singh <shivampesitbng@gmail.com>
2017-08-01 05:40:35 -0700

    glitch in show route fixed

--- src/components/7_SHOW_DIRECT_BUS_ROUTE.vue
+++ src/components/7_SHOW_DIRECT_BUS_ROUTE.vue
@@ -33,6 +33,69 @@ export default{
           return response.json();
         })
         .then(show_route => {
+          //console.log("show_route -> "+show_route);
+          if(show_route == null){
+
+            //
+            //console.log("_pehle -> "+bus_route);
+            let x = bus.route_name.indexOf("-");
+            //console.log("index of '-' ->" + x);
+            let str2 = bus.route_name.slice(x+1);
+            //console.log(str2);
+            let str1 = bus.route_name.slice(0,x);
+            //console.log(str1);
+            var str3 = str2 + '-' + str1;
+            //console.log("_baad me -> "+str3);
+            //
+
+            this.$http.get('bus_routes/'+str3+'.json')
+              .then(response => {
+                return response.json();
+              })
+              .then(show_route=>{
+                console.log(show_route);
+
+                //
+                let x = bus.no.indexOf("-");
+                let str = bus.no.slice(x+1);
+                //console.log("str -> "+str);
+
+                if(str == 'UP'){
+                  //console.log("UP");
+                  for(let r in show_route){
+                    if(this.$store.state.selected_source.name == show_route[r]){
+                      console.log("source -> " + show_route[r]);
+                      this.stop_name_arr.push("source -> " + show_route[r]);
+                      continue;
+                    }
+                    if(this.$store.state.selected_destination.name == show_route[r]){
+                      console.log("destination -> " + show_route[r]);
+                      this.stop_name_arr.push("destination -> " + show_route[r]);
+                      continue;
+                    }
+                    console.log(show_route[r]);
+                    this.stop_name_arr.push(show_route[r]);
+                  }
+                }else if(str == 'DN'){
+                  //console.log("DN");
+                  for(let r = show_route.length-1; r >= 0; r-- ){
+                    if(this.$store.state.selected_source.name == show_route[r]){
+                      console.log("source -> " + show_route[r]);
+                      this.stop_name_arr.push("source -> " + show_route[r]);
+                      continue;
+                    }
+                    if(this.$store.state.selected_destination.name == show_route[r]){
+                      console.log("destination -> " + show_route[r]);
+                      this.stop_name_arr.push("destination -> " + show_route[r]);
+                      continue;
+                    }
+                    console.log(show_route[r]);
+                    this.stop_name_arr.push(show_route[r]);
+                  }
+                }
+                return ;
+              })
+          }
 
           //
           let x = bus.no.indexOf("-");
@@ -57,7 +120,7 @@ export default{
             }
           }else if(str == 'DN'){
             //console.log("DN");
-            for(let r = show_route.length(); r >= 0; r-- ){
+            for(let r = show_route.length; r >= 0; r-- ){
               if(this.$store.state.selected_source.name == show_route[r]){
                 console.log("source -> " + show_route[r]);
                 this.stop_name_arr.push("source -> " + show_route[r]);
@@ -85,3 +148,9 @@ export default{
   }
 }
 </script>
+
+<style scoped>
+#main{
+  margin-top:-10%;
+}
+</style>
